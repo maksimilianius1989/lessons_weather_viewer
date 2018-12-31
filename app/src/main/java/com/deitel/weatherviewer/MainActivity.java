6b15b20e739b6d287283f5a2109c10e335cd5cfc
maks <it.vimax@gmail.com>
2018-12-31 06:15:14 +0200

    main activity create

--- app/src/main/java/com/deitel/weatherviewer/MainActivity.java
+++ app/src/main/java/com/deitel/weatherviewer/MainActivity.java
@@ -8,8 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Weather> weatherList = new ArrayList<>();
+    private WeatherArrayAdapter weatherArrayAdapter;
+    private ListView weatherListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +28,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        weatherListView = (ListView) findViewById(R.id.weatherListView);
+        weatherArrayAdapter = new WeatherArrayAdapter(this, weatherList);
+        weatherListView.setAdapter(weatherArrayAdapter);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText locationEditText = (EditText) findViewById(R.id.locationEditText);
+                URL url = createURL
             }
         });
     }
@@ -42,5 +56,20 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private createURL(String city) {
+        String apiKey = getString(R.string.api_key);
+        String baseUrl = getString(R.string.web_service_url);
+
+        try {
+            String urlString = baseUrl + URLEncoder.encode(city, "UTF-8") + "&units=metric&lang=ua&APPID=" + apiKey;
+            return new URL(urlString);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
